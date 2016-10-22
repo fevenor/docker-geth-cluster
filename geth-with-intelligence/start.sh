05402340f897676c51c77617d5d2aaf82abb684b
fevenor <fevenor@outlook.com>
2016-10-22 22:10:05 +0800

    update geth

--- geth-with-intelligence/start.sh
+++ geth-with-intelligence/start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-sed -ie "s/\INSTANCE_NAME_TEMP/$HOSTNAME/" /root/processes.json
+sed -ie "s/\INSTANCE_NAME_TEMP/$INSTANCE_NAME/" /root/processes.json
 sed -ie "s/\CONTACT_DETAILS_TEMP/$CONTACT_DETAILS/" /root/processes.json
 
 /usr/bin/pm2 start /root/processes.json
 sleep 3
-/geth $@
+/geth init=/root/genesis.json $@
